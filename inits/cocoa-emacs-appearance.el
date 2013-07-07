76b82f27e0b90d1c718fc4b92e7d9b4c59b9b2a4
Masahiro Kiyota <hiro@juzbox.com>
2013-07-07 13:01:35 +0900

    changed font & transparency

--- inits/cocoa-emacs-appearance.el
+++ inits/cocoa-emacs-appearance.el
@@ -1,6 +1,8 @@
-(set-default-font "Bitstream Vera Sans Mono-11")
-(set-face-font 'variable-pitch "Bitstream Vera Sans Mono-11")
+(set-default-font "Menlo-12")
+(set-face-font 'variable-pitch "Menlo-12")
 (set-fontset-font (frame-parameter nil 'font)
                   'japanese-jisx0208
                   '("ヒラギノ角ゴ Pro W3" . "unicode-bmp")
                   )
+
+(add-to-list 'default-frame-alist '(alpha . (0.85 0.85)))
